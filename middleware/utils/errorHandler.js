88091e9ce1f846ca92affcfb8620b75812544578
yravinderkumar33 <yravinderkumar33@gmail.com>
2022-05-31 20:19:12 +0530

    Issue #SB-29454 feat: debug log in global error handler

--- middleware/utils/errorHandler.js
+++ middleware/utils/errorHandler.js
@@ -1,9 +1,12 @@
+var debug = require('debug')('util:errorHandler');
+
 const { formatApiResponse, getDefaultParams } = require('../../helpers/responseFormatter');
 const CONSTANTS = require('../../resources/constants.json')
 
 module.exports = () => (err, req, res, next) => {
+    debug('Error', JSON.stringify(err));
     const statusCode = err.statusCode || 500;
-
+    
     const paramsObject = getDefaultParams({
         err: err.errorObject || null,
         errmsg: err.message || null,
